1b11e7bb31c71f09b1a7919e1a0e682a76cf395a
jarriztg <rickygamberroz@hotmail.com>
2016-03-23 15:52:33 -0600

    Updated

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,7 +14,7 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\nMod+ Plugins: @Jarriz \n\n Checkout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\nMod+ Plugins: @Jarriz \n\n More: https://github.com/jarriztg/DBTeammod\n.'
 end
 
 return {
