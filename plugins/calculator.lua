2d8e3e4b37a45abfd4203a12216775427aace373
jarriztg <rickygamberroz@hotmail.com>
2016-04-07 22:36:27 -0500

    Update calculator.lua

--- plugins/calculator.lua
+++ plugins/calculator.lua
@@ -25,7 +25,8 @@ return {
   description = "Calculate math expressions with mathjs API",
   usage = "!calc [expression]: evaluates the expression and sends the result.",
   patterns = {
-    "^[!/#]calcular (.*)$"
+    "^[!/#]calcular (.*)$",
+    "^[!/#]calc (.*)$"
   },
   run = run
 }
